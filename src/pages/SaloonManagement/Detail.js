81dbc6493fd7132c7b2cb43e110abbdb46943f26
anas6925 <98manas1217@gmail.com>
2021-08-09 13:53:10 +0500

    detail page

--- src/pages/SaloonManagement/Detail.js
+++ src/pages/SaloonManagement/Detail.js
@@ -2,14 +2,14 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
-
+import icon from '../../assets/images/Users/icon.png'
 
 
 class Detail extends Component {
     constructor(props) {
         super(props);
         this.state = {
-           
+
 
         };
 
@@ -44,10 +44,133 @@ class Detail extends Component {
         }
 
         return (
-            <div className="DashbordContainer">
+            
+           
+        <div className="detail-section">
+            <div className="container p-0">
+            <div className="col-md-12 p-0 pt-4">
+                <div className="text">
+                    <p className="poppins_medium">Salon Information</p>
+                </div>
             </div>
-        )
-    }
+
+
+<div className="row">
+    <div className="col-md-6">
+        <div className="left-div mt-4">
+            <p className="poppins_medium ">Salon Details &nbsp;&nbsp;&nbsp;
+            <img src={icon}/></p>
+            <p className="poppins_regular">About salon registration</p>
+        </div>
+    </div>
+
+
+    <div className="col-md-6">
+        <div className="left-div mt-4">
+            <p className="poppins_medium ">Contact Information &nbsp;&nbsp;&nbsp;
+            <img src={icon}/></p>
+            <p className="poppins_regular">Used for tracking your Salon</p>
+        </div>
+    </div>
+   
+</div>
+
+
+
+<div className="row mt-3">
+    <div className="col-md-6">
+        <div className="right-div text-right">
+            <p className="poppins_medium">Salon Name  &nbsp;&nbsp;
+             <input className="srch-box" type="search" placeholder="Rose Beauty Parlor"></input></p>
+             <p className="poppins_medium">Registration No.  &nbsp;&nbsp;
+             <input className="srch-box" type="search" placeholder="1569491"></input></p>
+        </div>
+    </div>
+    <div className="col-md-6">
+    <div className="right-div text-right">
+            <p className="poppins_medium"> Email  &nbsp;&nbsp;
+             <input className="srch-box" type="search" placeholder="98manas1217@gmail.com"></input></p>
+             <p className="poppins_medium">Phone No.&nbsp;&nbsp;
+             <input className="srch-box" type="search" placeholder="+92 332 4567712"></input></p>
+        </div>
+
+        
+    </div>
+
+    
+</div>
+
+<div className="row">
+    <div className="col-md-6">
+        <div className="left-div mt-5">
+            <p className="poppins_medium ">Address &nbsp;&nbsp;&nbsp;
+            <img src={icon}/></p>
+            <p className="poppins_regular">Salon address or branch details</p>
+        </div>
+    </div>
+
+
+    <div className="col-md-6">
+        <div className="left-div mt-5">
+            <p className="poppins_medium ">Company Status &nbsp;&nbsp;&nbsp;
+            <img src={icon}/></p>
+            <p className="poppins_regular">Salon running status</p>
+        </div>
+    </div>
+   
+</div>
+
+
+
+<div className="row mt-3">
+    <div className="col-md-6">
+        <div className="right-div text-right">
+            <p className="poppins_medium">Branch Name &nbsp;&nbsp;
+             <input className="srch-box" type="search" placeholder="KDA Market"></input></p>
+             <p className="poppins_medium">Street Address &nbsp;&nbsp;
+             <input className="srch-box" type="search" placeholder="Aone Cottages Street 9"></input></p>
+             <p className="poppins_medium">City Name &nbsp;&nbsp;
+             <input className="srch-box" type="search" placeholder="Karachi"></input></p>
+             <p className="poppins_medium">Country Name &nbsp;&nbsp;
+             <input className="srch-box" type="search" placeholder="Paksitan"></input></p>
+        </div>
+    </div>
+    <div className="col-md-6">
+    <div className="right-div text-right">
+            <p className="poppins_medium">Status&nbsp;&nbsp;
+             <select className="srch-box" >
+                 <option>open</option>
+                 <option>close</option>
+
+                 </select></p>
+             
+        </div>
+       
+    </div>
+    <div className="col-md-12 text-right">
+            <button className=" poppins_semibold save-btn">Save Information</button>
+        </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+    )
+}
 
 }
 
--- src/pages/SaloonManagement/Service.js
+++ src/pages/SaloonManagement/Service.js
@@ -2,14 +2,15 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
-
-
+import circlelogo from '../../assets/images/Users/seat.png'
+import search from '../../assets/images/Users/search.png'
+import print from '../../assets/images/Users/print.png'
 
 class Service extends Component {
     constructor(props) {
         super(props);
         this.state = {
-           
+
 
         };
 
@@ -44,7 +45,66 @@ class Service extends Component {
         }
 
         return (
-            <div className="DashbordContainer">
+            <div className="Service-Detail">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-5">
+                            <div className="col-md-5 white-box">
+                                <div className="row">
+                                    <div className="col-md-6 box-inner">
+                                        <p className="poppins_medium">50</p>
+                                        <br /><p className="poppins_regular">Total<br />Services</p>
+                                    </div>
+                                    <div className="col-md-6 ">
+                                        <div className="circle">
+                                            <img className="image" src={circlelogo} /></div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-2"></div>
+                        <div className="col-md-5  color ">
+                            <img className="  search-icon" src={search} />
+                            <input className=" poppins_regular search" type="search" placeholder="Search Here"></input>
+                            <button className=" poppins_medium srch-btn">Search</button>
+                            <div className="row m-0">
+                                <button className=" poppins_regular add-new">Add New</button>
+                                <button className=" poppins_regular bulk-action">Bulk Action</button>
+                                <p className="report poppins_regular text-right">Print Report&nbsp;&nbsp;<label><img src={print} /></label></p>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                </div>
             </div>
         )
     }
